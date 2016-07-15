cd5eafc24ff56f43dd97a9c1d4ad849c74cb6196
zhaojun <zhj565405576@163.com>
2016-07-15 15:37:41 +0800

    后台，表

--- WebContent/WEB-INF/jsp/hello.jsp
+++ WebContent/WEB-INF/jsp/hello.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<span>${sayHello}</span>
+	<span>--------------------</span>
 </body>
 </html>
--- src/com/snaillove/standard/controller/SystemController.java
+++ src/com/snaillove/standard/controller/SystemController.java
@@ -2,12 +2,15 @@ package com.snaillove.standard.controller;
 
 
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.snaillove.standard.model.Protocol;
 import com.snaillove.standard.service.SystemService;
 
 
@@ -17,7 +20,9 @@ public class SystemController {
 	@RequestMapping(value="/hello")
 	public String sayHello(){
 		System.out.print("-------------------");
+		List<Protocol> list = systemService.queryProtocol();
 		System.out.println(systemService.queryProtocol());
+		
 		return "/hello";
 	}
 	
